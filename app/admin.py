b2e61669331a2455217ffb1031fd3c4a67ca525b
jay <wuj20061224@gmail.com>
2022-08-23 12:57:57 +0800

    first

---
+++ app/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import stock_deal
+
+# Register your models here.
+
+admin.site.register(stock_deal)
---
+++ app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+
+class stock_deal(models.Model):
+	company_code = models.CharField(default='null', max_length=15)
+	company_name = models.CharField(default='null', max_length=6)
+	share = models.IntegerField()
+	bought_date = models.DateField()
+	bought_price = models.FloatField()
+	current_price = models.FloatField()
+	profit_and_loss = models.FloatField()
